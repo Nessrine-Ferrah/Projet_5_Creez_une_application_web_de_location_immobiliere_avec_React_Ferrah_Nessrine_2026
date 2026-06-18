9797ceb3a6fa3b98cc445af86583420ddda4f8af
Ferrah Nessrine <nessrineferrah01@hotmail.com>
2026-06-18 05:42:07 +0200

    Implémentation du composant LogementHeader : affichage du titre et de la localisation du logement, génération dynamique des tags via map(), structure HTML conforme à la maquette (titre, paragraphe, tags), ajout du style associé (mise en page, couleurs, flex, arrondis).

--- projet-kasa/src/components/LogementHeader/LogementHeader.jsx
+++ projet-kasa/src/components/LogementHeader/LogementHeader.jsx
@@ -1,7 +1,20 @@
 import styles from './LogementHeader.module.scss'
 
-const LogementHeader = () => {
+const LogementHeader = ({title, location, tags}) => {
+    return (
+        <div className={styles.contentHeader}>
+            <div className={styles.textHeader}>
+                <h1 className={styles.titleHeader}>{title}</h1>
+                <p className={styles.paragHeader}>{location}</p>
+            </div>
+            <div className={styles.tagsHeader}>
+                {tags.map((tag, index)=>(
+                    <p className={styles.contentTags} key={index}>{tag}</p>
+                ))}
+            </div>
+        </div>
 
+    )
 }
 
 export default LogementHeader

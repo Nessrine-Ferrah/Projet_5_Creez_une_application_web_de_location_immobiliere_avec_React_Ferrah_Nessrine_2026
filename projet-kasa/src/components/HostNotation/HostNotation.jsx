f01965b9e5700d671503c00f1d3f3d23a3dd282c
Ferrah Nessrine <nessrineferrah01@hotmail.com>
2026-06-18 05:38:30 +0200

    Création du composant HostNotation : affichage du nom de l’hôte sur deux lignes via replace() et white-space: pre-line, intégration de la photo de l’hôte avec style responsive et border-radius 50%, génération dynamique des 5 étoiles avec comparaison index < rating, utilisation des icônes starOrange et starGrey selon la notation et mise en place de la structure et du style (flex, alignements, spacing).

--- projet-kasa/src/components/HostNotation/HostNotation.jsx
+++ projet-kasa/src/components/HostNotation/HostNotation.jsx
@@ -1,7 +1,26 @@
 import styles from './HostNotation.module.scss'
+import starOrange from '../../assets/starOrange.png'
+import starGrey from '../../assets/starGrey.png'
 
-const HostNotation = () => {
-
+const HostNotation = ({ host, rating }) => {
+    return (
+        <div className={styles.contentHost}>
+            <div className={styles.contentPicture}>
+                <p className={styles.nameHost}>{host.name.replace(" ", "\n")}</p>
+                <img src={host.picture} 
+                alt={`Photo de ${host.name}`}
+                className={styles.pictureHost}/>
+            </div>
+            <div className={styles.startNotation}>
+                {[...Array(5)].map((_, index) => (
+                    <span key={index} className={styles.star}>
+                        {index < rating ? <img src={starOrange} alt="Nombre d'étoiles orange sur 6"/> 
+                        :<img src={starGrey} alt="Nombre d'étoiles gris"/>}
+                    </span>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default HostNotation
